4168b3fb95793a8eced815bf7bf7f5dacce1c7d1
Caique Godoy <caiquecgodoy@gmail.com>
2024-06-04 19:56:37 -0300

    bora la

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from time import sleep
 
-def primeira():
-    print('primeira')
-    sleep(2)
-
-def segunda():
-    print('segunda')
-    sleep(2)
-
-def terceira():
-    print('terceira')
-    sleep(2)
+from airflow.decorators import dag, task
+from datetime import datetime
 
 
+
+@dag(
+        dag_id='dag_teste',
+        description='meu teste',
+        schedule='* * * 1 *',
+        start_date=datetime(2024,6,4),
+        catchup=False
+)
 def pipeline():
-    primeira()
-    segunda()
-    terceira()
+    @task
+    def primeira():
+        print('primeira')
+        sleep(2)
 
+    @task
+    def segunda():
+        print('segunda')
+        sleep(2)
+
+    @task
+    def terceira():
+        print('terceira')
+        sleep(2)
+
+    t1 = primeira()
+    t2 = segunda()
+    t3 = terceira()
+
+    t1 >> t2 >> t3
 
 pipeline()
